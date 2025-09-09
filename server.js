e1272161f525e229911caf7465d8137b1fb8e49a
lucasbaruj4 <barujalucas0@gmail.com>
2025-09-09 19:45:42 -0300

    empty folders for structure

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import { v4 as uuidv4} from 'uuid'
 const app = express()
 const PORT = 3000 
 
-const jobs = new Map()
+const jobs = new Map() 
 
 const upload = multer({
   dest: 'uploads/', // Temporary local storage folder
@@ -40,18 +40,92 @@ app.post('/api/upload', upload.single('video'), async (req, res) => {
         progress: 0,
         input: req.file.path, 
         originalname: req.file.originalname,
+        totalQualities: 0,
+        completedQualities: 0,
         outputs: {}
     });
 
-    // TODO: transcoding logic, we'll do this next
+    
+    transcodeVideo(jobID, req.file.path).catch(err => {
+        const job = jobs.get(jobID);
+        if (job) {
+            job.status = 'failed';
+            job.error = err.message;
+        }
+    });
 
     res.json({
-        jobId,
-        message: 'Video uplaoded successfully', 
+        jobID,
+        message: 'Video uploaded successfully', 
         status: 'pending'
     });
 });
 
+async function transcodeVideo(jobID, inputPath) {
+    const job = jobs.get(jobID);
+
+    const outputDir = path.join('transcoded', jobID);
+    await fs.mkdir(outputDir, {recursive: true});
+
+    const qualities = [
+        { name: '1080p', width: 1920, height: 1080, bitrate: '5000k'},
+        { name: '720p', width: 1280, height: 720, bitrate: '2500k'},
+        { name: '360p', width: 640, height: 360, bitrate: '1000k'}
+
+    ];
+
+    job.status = 'processing';
+    job.totalQualities = qualities.length;
+    job.completedQualities = 0;
+
+    for (const quality of qualities) {
+        const outputPath = path.join(outputDir, `${quality.name}.mp4`);
+
+        await new Promise((resolve, reject) => {
+            ffmpeg(inputPath)
+            .outputOptions([
+                `-vf scale=${quality.width}:${quality.height}`,
+                `-b:v ${quality.bitrate}`,
+                `-b:a 128k`,
+                `-c:v libx264`,
+                `-c:a aac`,
+                `-preset medium`,
+                `-movflags +faststart`
+            ])
+            .on('progress', (progress) => {
+                const qualityProgress = job.completedQualities * 100;
+                const currentProgress = progress.percent || 0;
+                job.progress = Math.round(
+                    (qualityProgress + currentProgress) / job.totalQualities
+                );
+                console.log(`Job ${jobID}: ${job.progress}% complete`)
+            })
+            .on('end', () => {
+                job.completedQualities++;
+                job.outputs[quality.name] = outputPath;
+                console.log(`${quality.name} complete for job ${jobID}`);
+                resolve();
+            })
+            .on('error', (err) => {
+                console.error(`Error transcoding quality: ${quality.name}.`, err)
+                reject(err)
+            })
+            .save(outputPath)
+        })
+
+    }
+
+    job.status = 'completed';
+    job.progress = 100;
+
+    await fs.unlink(inputPath);
+
+    console.log(`Job ${jobID} fully completed`);
+}
+
 app.listen(PORT, () => {
     console.log(`StreamForge running on http://localhost:${PORT}`)
 });
+
+
+    
